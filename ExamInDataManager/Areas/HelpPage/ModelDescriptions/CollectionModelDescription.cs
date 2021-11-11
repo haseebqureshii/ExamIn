415dc5694fda3d54bb7b614ff4930d59474d7cb9
Haseeb Qureshi <76586698+haseebqureshii@users.noreply.github.com>
2021-11-12 03:54:03 +0500

    Add the WebApi Project
    Added a blank WebApi project and some tweaks.

---
+++ ExamInDataManager/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ExamInDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ExamInDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExamInDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
