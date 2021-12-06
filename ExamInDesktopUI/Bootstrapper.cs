0d280df8d5f0f4f08f3d1bc0767d086458a49620
Haseeb Qureshi <76586698+haseebqureshii@users.noreply.github.com>
2021-12-06 20:21:20 +0500

    Login form created.
    Login form creation with username, password and login button.

--- ExamInDesktopUI/Bootstrapper.cs
+++ ExamInDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using ExamInDesktopUI.Helpers;
 using ExamInDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ExamInDesktopUI
 {
@@ -16,6 +18,10 @@ namespace ExamInDesktopUI
         public Bootstrapper()
         {
             Initialize();
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
         protected override void Configure()
