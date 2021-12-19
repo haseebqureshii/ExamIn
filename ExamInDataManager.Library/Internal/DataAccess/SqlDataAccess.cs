fcc52a1d1cf7f8a42920ff4923ed606ed4f37dbb
Haseeb Qureshi <76586698+haseebqureshii@users.noreply.github.com>
2021-12-20 00:17:43 +0500

    Added DataManager & DesktopUI libraries, to access user data from database.

---
+++ ExamInDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamInDataManager.Library.Internal.DataAccess
+{
+    internal class SqlDataAccess
+    {
+        public string GetConnectionString(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedure, U parameters, string connectionStringName)
+        {
+            string conectionString = GetConnectionString(connectionStringName);
+            
+            using (IDbConnection connection = new SqlConnection(conectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedure, parameters, 
+                    commandType: CommandType.StoredProcedure).ToList();
+
+                return rows;
+            }
+        }
+
+        public void SaveData<T>(string storedProcedure, T parameters, string connectionStringName)
+        {
+            string conectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(conectionString))
+            {
+                connection.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
